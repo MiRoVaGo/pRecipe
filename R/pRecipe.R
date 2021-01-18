4f843416c3312a808fe2101cf3ef96b72c7d5ed3
Mijael Vargas <mirovago@gmail.com>
2021-01-18 14:47:03 +0100

    added import and subset functions

--- R/pRecipe.R
+++ R/pRecipe.R
@@ -29,10 +29,10 @@
 #' \item{"trmm_3b43" for TRMM 3B43 v7,}
 #' \item{"udel" for UDEL v501.}
 #' }
-#' @param reformat logical. If TRUE (default) the downloaded datasets are reformatted into data.table and stored in .Rds files
+#' @param reformat logical. If TRUE the downloaded datasets are reformatted into data.table and stored in .Rds files
 #' @export
 
-download_data <- function(destination, name = "all", reformat = TRUE){
+download_data <- function(destination, name = "all", reformat = FALSE){
   if (!Reduce("&", is.element(name, c("20cr", "all", "cmap", "cpc", "cru_ts", "ghcn", "gpcc", "gpcp", "gpm_imergm", "ncep_ncar", "ncep_doe", "precl", "trmm_3b43", "udel")))){
     stop("Error: Data set not supported. Select from 20cr, cmap, cpc, cru_ts, ghcn, gpcc, gpcp, gpm_imergm, ncep_ncar, ncep_doe, precl, trmm_3b43, udel")
   }
@@ -82,8 +82,8 @@ download_data <- function(destination, name = "all", reformat = TRUE){
 #' @param folder_path a character string with the path where the "raw" folder is located.
 #' @export
 
-reformat_data <- function(folder_path, name){
-  if (!Reduce("&", is.element(name, c("20cr", "all", "cmap", "cpc", "cru_ts", "ghcn", "gpcc", "gpcp", "gpm_imergm", "ncep_ncar", "ncep_doe", "precl", "trmm_3b43", "udel")))){
+reformat_data <- function(folder_path, name = "all"){
+  if (!Reduce("&", is.element(name, c("20cr", "cmap", "cpc", "cru_ts", "ghcn", "gpcc", "gpcp", "gpm_imergm", "ncep_ncar", "ncep_doe", "precl", "trmm_3b43", "udel")))){
     stop("Error: Data set not supported. Select from 20cr, cmap, cpc, cru_ts, ghcn, gpcc, gpcp, gpm_imergm, ncep_ncar, ncep_doe, precl, trmm_3b43, udel")
   }
   lapply(name, function(dataset) switch(dataset,
@@ -111,7 +111,7 @@ reformat_data <- function(folder_path, name){
 #'
 #' @param name a character string with the name of the desired data set. Suitable options are:
 #' \itemize{
-#' \item{"all" for all of the below listed data sets (default),}
+#' \item{"all" for all of the below listed data sets,}
 #' \item{"20cr" for 20CR v3,}
 #' \item{"cmap" for CMAP standard version,}
 #' \item{"cpc" for CPC-Global,}
@@ -127,90 +127,37 @@ reformat_data <- function(folder_path, name){
 #' \item{"udel" for UDEL v501.}
 #' }
 #' @param folder_path a character string with the path where the "database" folder is located.
+#' @return a data.table with the requested precipitation data sets.
 #' @export
 
 import_data <- function(folder_path, name){
   if (!Reduce("&", is.element(name, c("20cr", "all", "cmap", "cpc", "cru_ts", "ghcn", "gpcc", "gpcp", "gpm_imergm", "ncep_ncar", "ncep_doe", "precl", "trmm_3b43", "udel")))){
     stop("Error: Data set not supported. Select from 20cr, cmap, cpc, cru_ts, ghcn, gpcc, gpcp, gpm_imergm, ncep_ncar, ncep_doe, precl, trmm_3b43, udel")
   }
-  if (name == "all") name <- c("20cr", "cmap", "cpc", "cru_ts", "ghcn", "gpcc", "gpcp", "gpm_imergm", "ncep_ncar", "ncep_doe", "precl", "trmm_3b43", "udel")
-  lapply(name, function(dataset) switch(dataset,
-                                        "20cr" = {p20cr <<- readRDS(paste0(folder_path, "/20cr.Rds"))},
-                                        "cmap" = {cmap <<- readRDS(paste0(folder_path, "/cmap.Rds"))},
-                                        "cpc" = {cpc <<- readRDS(paste0(folder_path, "/cpc.Rds"))},
-                                        "cru_ts" = {cru_ts <<- readRDS(paste0(folder_path, "/cru_ts.Rds"))},
-                                        "ghcn" = {ghcn <<- readRDS(paste0(folder_path, "/ghcn.Rds"))},
-                                        "gpcc" = {gpcc <<- readRDS(paste0(folder_path, "/gpcc.Rds"))},
-                                        "gpcp" = {gpcp <<- readRDS(paste0(folder_path, "/gpcp.Rds"))},
-                                        "gpm_imergm" = {gpm_imergm <<- readRDS(paste0(folder_path, "/gpm_imergm.Rds"))},
-                                        "ncep_ncar" = {ncep_ncar <<- readRDS(paste0(folder_path, "/ncep_ncar.Rds"))},
-                                        "ncep_doe" = {ncep_doe <<- readRDS(paste0(folder_path, "/ncep_doe.Rds"))},
-                                        "precl" = {precl <<- readRDS(paste0(folder_path, "/precl.Rds"))},
-                                        "trmm_3b43" = {trmm_3b43 <<- readRDS(paste0(folder_path, "/trmm_3b43.Rds"))},
-                                        "udel" = {udel <<- readRDS(paste0(folder_path, "/udel.Rds"))}
-  ))
-}
-
-#' Select precipitation data sets
-#'
-#' The function \code{select_data} imports the requested data sets.
-#'
-#' @param name a character string with the name of the desired data set. Suitable options are:
-#' \itemize{
-#' \item{"all" for all of the below listed data sets (default),}
-#' \item{"20cr" for 20CR v3,}
-#' \item{"cmap" for CMAP standard version,}
-#' \item{"cpc" for CPC-Global,}
-#' \item{"cru_ts" for CRU_TS v4.04,}
-#' \item{"ghcn" for GHCN-M v2}
-#' \item{"gpcc" for GPCC v2018,}
-#' \item{"gpcp" for GPCP v2.3,}
-#' \item{"gpm_imergm" for GPM IMERGM Final v06,}
-#' \item{"ncep" for NCEP/NCAR,}
-#' \item{"ncep2" for NCEP/DOE,}
-#' \item{"precl" for PRECL,}
-#' \item{"trmm_3b43" for TRMM 3B43 v7,}
-#' \item{"udel" for UDEL v501.}
-#' }
-#' @param folder_path a character string with the path where the "database" folder is located.
-#' @return a list with the paths to the data sets selected
-#' @export
-
-select_data <- function(folder_path, name){
-  if (!Reduce("&", is.element(name, c("20cr", "all", "cmap", "cpc", "cru_ts", "ghcn", "gpcc", "gpcp", "gpm_imergm", "ncep_ncar", "ncep_doe", "precl", "trmm_3b43", "udel")))){
-    stop("Error: Data set not supported. Select from 20cr, cmap, cpc, cru_ts, ghcn, gpcc, gpcp, gpm_imergm, ncep_ncar, ncep_doe, precl, trmm_3b43, udel")
+  if (name == "all"){
+    name <- list.files(folder_path, full.names = TRUE)
+  } else {
+    
+    name <- grep(paste(name, collapse = "|"), list.files(folder_path, full.names = TRUE), value = TRUE)
   }
-  if (name == "all") name <- c("20cr", "cmap", "cpc", "cru_ts", "ghcn", "gpcc", "gpcp", "gpm_imergm", "ncep_ncar", "ncep_doe", "precl", "trmm_3b43", "udel")
-  name <- paste0(folder_path, "/", name, ".Rds") %>% as.list()
+  precip <- lapply(name, readRDS) %>% rbindlist()
+  class(precip) <- append(class(precip),"pRecipe")
+  return(precip)
 }
 
 #' Subset precipitation data sets
 #'
-#' The function \code{subset_data} imports the requested data sets.
+#' The function \code{subset_data} subsets the imported data sets.
 #'
-#' @param data_list a list generated by \code{select_data}
+#' @param x a pRecipe data.table imported using \code{import_data}.
 #' @param start_year numeric.
 #' @param end_year numeric.
-#' @param country shp file.
-#' @return a list with the subsetted data sets
+#' @param box numeric vector. Bounding box in the form: (xmin, ymin, xmax, ymax).
+#' @return a data.table with the subsetted data sets
 #' @export
 
-subset_data <- function(data_list, start_year, end_year, country){
-  data(wrld_simpl, package = "maptools")
-  country <- subset(wrld_simpl, ISO3 == country)
-  data_names <- sub(".*/database/", "", data_list)
-  data_names <- substr(data_names,1,nchar(data_names)-4)
-  precip <- lapply(data_list, function(name){
-    dummie_raster <- readRDS(name)
-    dummie_name <- dummie_raster$name[1]
-    dummie_raster <- dummie_raster[year(Z) >= start_year & year(Z) <= end_year, 1:4]
-    dummie_raster <- data.table::dcast(dummie_raster, x + y ~ Z)
-    sp::coordinates(dummie_raster) <- ~ x + y
-    sp::gridded(dummie_raster) <- TRUE
-    dummie_raster <- raster::brick(dummie_raster)
-    dummie_raster <- raster::crop(dummie_raster, raster::extent(country))
-    return(assign(dummie_name, dummie_raster))
-  })
-  names(precip) <- data_names
-  return(precip)
+subset_data <- function(x, start_year, end_year, box){
+  if (!is(x, "pRecipe")) stop("Error: x must be a pRecipe data.table")
+  x <- x[year(Z) >= start_year & year(Z) <= end_year & x >= box[1] & x <= box[3] & y >= box[2] & y <= box[4]]
+  return(x)
 }
