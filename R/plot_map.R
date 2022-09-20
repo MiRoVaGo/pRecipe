374dc59ea5e15e984c8d794f9e8523e2821b6c7d
Mijael Vargas <mirovago@gmail.com>
2022-09-20 13:58:06 +0200

    Improved map aesthetics

--- R/plot_map.R
+++ R/plot_map.R
@@ -6,6 +6,7 @@
 #' @importFrom grDevices dev.size
 #' @importFrom methods as is
 #' @importFrom raster as.data.frame
+#' @importFrom stats quantile
 #' @param dummie a .nc file with precipitation
 #' @return ggplot object
 #' @export
@@ -16,25 +17,38 @@ plot_map <- function(dummie){
   }
   dummie <- as.data.frame(dummie, xy = TRUE, long = TRUE, na.rm = TRUE)
   dummie <- as.data.table(dummie)
+  if (round((max(dummie$x) - min(dummie$x))) == 360){
+    map_expand <- FALSE
+  } else {
+    map_expand <- TRUE
+  }
+  map_max <- quantile(dummie$value,0.9995)
+  map_min <- round(min(dummie$value)) -1 
+  if (map_min < 0){
+    map_min <- 0
+  }
   p00 <- ggplot(dummie, aes(x = x, y = y)) +
     geom_raster(aes(fill = value)) +
     borders(colour = "black") +
     theme_bw() +
     coord_cartesian(xlim = c(min(dummie$x), max(dummie$x)), 
-                    ylim = c(min(dummie$y), max(dummie$y))) +
+                    ylim = c(min(dummie$y), max(dummie$y)),
+                    expand = map_expand) +
     labs(x = NULL, y = NULL, fill = "[mm]", title = format(as.Date(dummie$Z[1]), "%B %Y")) +
-    scale_fill_distiller(palette = "Spectral",
-                         breaks = seq(min(dummie$value), max(dummie$value), 
-                                      round(max((dummie$value) - min(dummie$value))/8)),
+    scale_fill_distiller(palette = "YlGnBu",
+                         direction = 1,
+                         na.value = "#081D58",
+                         limits = c(map_min, map_max),
                          guide = guide_colorbar(frame.colour = "black",
                                                 ticks.colour = "black")) +
     theme(panel.border = element_rect(colour = "black", size = 2),
           panel.grid = element_blank(), plot.tag = element_text(size = 24), 
-          plot.title = element_text(size=28), axis.text = element_text(size = 20), 
+          plot.title = element_text(size = 28),
+          axis.text = element_text(size = 20), 
           axis.title = element_text(size = 24), 
           legend.text = element_text(size = 20), 
           legend.title = element_text(size = 24), 
-          axis.ticks.length = unit(-.25, "cm"),
+          axis.ticks.length = unit(-0.25, "cm"),
           legend.key.height = unit(dev.size()[2]/10, "inches"))
   return(p00)
 }
